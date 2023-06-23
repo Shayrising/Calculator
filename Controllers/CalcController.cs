4fb513278c3e2bfa496a7f8630daf5475f75ea32
Shayrising <shannonrising840@gmail.com>
2023-06-23 14:48:40 -0700

    First commit

---
+++ Controllers/CalcController.cs
@@ -0,0 +1,43 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalcController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new Calc());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Calc calculator, string calculate)
+        {
+            if (calculate == "div" && calculator.Number2 == 0)
+            {
+                calculator.Total = 0;
+            }
+            else
+            {
+                switch (calculate)
+                {
+                    case "add":
+                        calculator.Total = calculator.Number1 + calculator.Number2;
+                        break;
+                    case "sub":
+                        calculator.Total = calculator.Number1 - calculator.Number2;
+                        break;
+                    case "mult":
+                        calculator.Total = calculator.Number1 * calculator.Number2;
+                        break;
+                    case "div":
+                        calculator.Total = calculator.Number1 / calculator.Number2;
+                        break;
+                }
+            }
+
+            return View(calculator);
+        }
+    }
+}
+
---
+++ Models/Calc.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculator.Models
+{
+    public class Calc
+    {
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public double Total { get; set; }
+    }
+}
